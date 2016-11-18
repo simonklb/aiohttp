9d09a043d8b0c8d536419b9871b6cc6059043f4b
Simon Kollberg <simonkollberg@gmail.com>
2016-11-18 13:16:47 +0100

    Make message argument optional for BadHttpMessage exception

--- aiohttp/errors.py
+++ aiohttp/errors.py
@@ -106,7 +106,7 @@ class BadHttpMessage(HttpProcessingError):
     code = 400
     message = 'Bad Request'
 
-    def __init__(self, message, *, headers=None):
+    def __init__(self, message='', *, headers=None):
         super().__init__(message=message, headers=headers)
 
 
